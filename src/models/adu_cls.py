4e87ff78e0eb55e56606d00d33c9e1f5f39f3d55
Telmooo <telmoabaptista@hotmail.com>
2022-06-08 12:24:36 +0100

    Deleted old unused scripts

--- src/models/adu_cls.py
+++
@@ -1,7 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForPreTraining, AutoModel
-import torch.nn as nn
-import torch.functional as F
-
-class ADUCls(nn.Module):
-    def __init__(self) -> None:
-        super().__init__()
